9ef7a92636b7ced6ceec9ef587fc004776f0f1d4
Jose Luis Garcia Tucci <98712473+jlgarciatucci@users.noreply.github.com>
2024-10-20 16:49:39 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setup(
     name="style_table",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=["pandas"],
     include_package_data=True,
