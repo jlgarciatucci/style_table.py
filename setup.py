6a2c3431570705466da1264f9d9f20d97b9cc147
Jose Luis Garcia Tucci <98712473+jlgarciatucci@users.noreply.github.com>
2024-10-20 16:05:52 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 setup(
     name="style_table",
     version="0.1.0",
@@ -7,9 +9,8 @@ setup(
     install_requires=["pandas"],
     include_package_data=True,
     description="A library for styling pandas DataFrames in Jupyter Notebooks",
-    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/your_library",
+    url="https://github.com/yourusername/style_table.py",
     author="Jose Luis Garcia Tucci",
     author_email="jlgarciatucci@gmail.com",
     license="MIT",
